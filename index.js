895e3a1be991152be91e3f6ea59c83636a40cad6
Sean <seanms418@gmail.com>
2022-01-28 19:59:55 +1100

    started pseudo code and installed inquirer

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// TODO: 
+// Using a command-line application that accepts user input that prompts for information about an application
+// repository that generates a README.md with the following sections
+//      Title of the project
+//      Sections: entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests,
+//      and Questions
+// The title of the project will be the title of the README generated
+// Have a list of options for the license choice that adds a badge for that license near the top of the README
+// and add a notice to the License section that explains which license the application is covered under
+// Prompt for the GitHub username and ass to the section of the README entitled Questions, with a link to the
+// GitHub profile
+// Prompt for an email address and add it to the section of the README entitled Questions, with instructions on
+// how to reach me with additional questions
+// Make the links in the Table of Contents take you to the corresponding section of the README
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
